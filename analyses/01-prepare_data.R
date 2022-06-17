1bd992f9cd8ae74935049a24fd35c1c194e96d87
Jonathan Peelle <jpeelle@Jonathans-MacBook-Air.local>
2022-06-17 11:27:48 -0600

    Reasonable working plots and model

--- analyses/01-prepare_data.R
+++ analyses/01-prepare_data.R
@@ -15,120 +15,38 @@ df <- read.csv("../data/motion_data.csv")
 
 df <- df[df$x!=0,]
 
+
+
 # add scan counter
 df <- df %>% group_by(subject_number) %>% mutate(scan = row_number(subject_number))
 
 
 # read in demographic data and add age, sex, age group to main df
-#df_demo <- read.csv("../data/participants.csv")
+dfp <- read.csv("../data/participants.csv")
+
+
+#---- get information from participants file to motion file ----
+
+dfj <- left_join(df, dfp, 
+                 by = c("subject_number", "FIRMM"),
+              keep = FALSE)
+
+
+# remove where we don't have demographic info
+dfj <- dfj[!is.na(dfj$age_group),]
+
+# write
+
+write.table(dfj, file = "data_filtered.csv", sep=",", row.names = FALSE)
 
 
 
-#---- summarize by session ----
+#---- summarize by run/subject ----
 
 dfrun <- summarize(group_by(df, subject_number, run),
                  FIRMM=mean(FIRMM),
                  mean=mean(FD),
                  SD=sd(FD))
-
-
-dfsubject <- summarize(group_by(df, subject_number),
-                   FIRMM=mean(FIRMM),
-                   meanFD=mean(FD),
-                   medianFD=median(FD),
-                   SDFD=sd(FD))
-
-
-write.table(dfsubject, file = "data_by_subject.csv", sep =",", row.names = FALSE)
-
-
-# 
-# ggbarplot(dfleft, x = "condition", y = "activity",
-#                 ylim = c(-5, 12),
-#                 add = c("mean_se", "jitter"),
-#                 color = "agegroup.x",
-#                 fill = "agegroup.x", alpha = 0.1,
-#                 palette = c("#339933", "#FF9933"),
-#                 position = position_dodge(0.8))
-#    
-
-#---- plot FD for 1-2 subjects ----
-
-df40 <- df[df$subject_number=="PL00040",]
-
-
-
-
-
-ggplot(data = df40, aes(x=scan, y=FD)) +
-         geom_line()
-
-
-
-#---- plot FD for FIRMM vs no FIRMM ----
-
-ggbarplot(dfsubject, x = "FIRMM", y = "meanFD",
-          add = c("mean_se", "jitter"),
-          position = position_dodge(0.8))
-
-
-#---- plot FD over time ----
-
-
-# # left
-# 
-# dfall <- dfal[dfal$ear=="L",]
-# 
-# gdl <- dfall %>%
-#   group_by(agegroup, frequency) %>%
-#   summarize(threshold = mean(threshold))
-# 
-# ggplot(data = dfall, aes(x = frequency, y = threshold, group = subj, color = agegroup)) + 
-#   geom_line(alpha = 0.5, size = 0.3) +
-#   geom_line(data = gdl, aes(x = frequency, y = threshold, group = agegroup), size = 2) +
-#   theme_classic() + 
-#   labs(title = "Left", x = "Frequency (Hz)", y = "Threshold (dB HL)") +
-#   scale_x_continuous(trans="log2") + scale_x_continuous(breaks=c(500, 2000, 4000, 8000)) +
-#   scale_y_reverse() + 
-#   color_palette(palette = c("#FF9933", "#339933")) +
-#   ylim(85, -10) +
-#   theme(legend.position = "none")  
-
-
-dfmean <- df %>%
-  group_by(FIRMM, scan) %>%
-  summarize(meanFD = mean(FD))
-
-
-# hack for now to get rid of weird big number of scans
-dfmean <- dfmean[dfmean$scan<800,]
-
-
-ggplot(data = dfmean, aes(x = scan, y = meanFD, group = FIRMM, color = FIRMM)) +
-  geom_line()
-
-
-# todo:
-
-# - remove gridlines, add indicators for each run, plot single subjects for supplemental
-
-
-
-
-
-#---- model ----
-
-
-# m1 <- lmer(FD ~ FIRMM * scan +
-#              (1 | scan) +
-#              (1 | run),
-#            data = df, )
-# 
-# summary(m1)
-
-# add age group
-
-
 
 
 
---
+++ analyses/02-plot.R
@@ -0,0 +1,118 @@
+
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+library(ggridges)
+
+feedbackColor = "green"
+nofeedbackColor = "gray40"
+
+
+#---- read data ----
+
+df <- read.csv("data_filtered.csv")
+dfp <- read.csv("../data/participants.csv")
+
+
+dfsubject <- group_by(df, subject_number) %>%
+  summarize(meanFD=mean(FD),
+            medianFD=median(FD),
+            SDFD=sd(FD))
+
+dfp <- dfp[!is.na(dfp$subject_number),]
+
+dfsubject <- left_join(dfp, dfsubject, 
+                       by = "subject_number",
+                       keep = FALSE)
+
+# get rid of NAs (people in dfp but not df)
+dfsubject <- dfsubject[!is.na(dfsubject$meanFD),]
+
+dfsubject$FIRMM <- as.factor(dfsubject$FIRMM)
+
+
+
+# order df by FIRMM (important for ggridges)
+
+
+
+
+
+
+
+#---- how many scans per subject? this varies... ----
+
+
+
+
+
+#---- plot FD over time for 2 subjects ----
+
+df16 <- df[df$subject_number=="sub-16",]  # sub-16 is our first nofeedback young adult
+df22 <- df[df$subject_number=="sub-22",] # sub-22 is our first feedback young adult
+
+p1 <- ggplot() +
+  geom_line(data = df16, aes(x=scan, y=FD), color = nofeedbackColor) +
+  geom_line(data = df22, aes(x=scan, y=FD), color = feedbackColor) +
+  theme_classic() +
+  xlab("Frame") +
+  xlim(0,800)
+
+
+
+
+#---- plot mean FD over time for all subjects (grouped by FIRMM) ----
+
+dfmean <- df %>%
+  group_by(FIRMM, scan) %>%
+  summarize(meanFD = mean(FD))
+
+dfmean$FIRMM <- as.factor(dfmean$FIRMM)
+
+# hack for now to get rid of weird big number of scans
+dfmean <- dfmean[dfmean$scan<800,]
+
+
+p2 <- ggplot(data = dfmean, aes(x = scan, y = meanFD, group = FIRMM, color = FIRMM)) +
+  geom_line() +
+  scale_colour_discrete(type = c(nofeedbackColor, feedbackColor)) + 
+  xlim(0, 800) + 
+  theme_classic() +
+  theme(legend.position = "none") +
+  xlab("Frame") +
+  ylab("Mean FD")
+
+
+
+#---- plot mean FD for FIRMM vs no FIRMM, summarized (one point per subject) ----
+
+p3 <- ggbarplot(dfsubject, x = "age_group", y = "meanFD",
+          add = c("mean_se", "jitter"),
+          error.plot = "upper_errorbar",
+          color = "FIRMM",
+          fill = "FIRMM", alpha = 0.1,
+          palette = c(nofeedbackColor, feedbackColor),
+          position = position_dodge(0.8),
+          xlab = "Age Group",
+          ylab = "Mean FD")
+  
+
+#---- combine these three plots (p1, p2, p3) into a single plot ----
+
+p12 <- ggarrange(p1, p2,
+          ncol = 1)
+          
+ggarrange(p12, p3,
+          ncol = 2)          
+
+
+
+
+#---- ridge plots of individual subjects ----
+
+ggplot(dff, aes(x = FD, y = subject_number, fill = FIRMM, height = stat(density))) +
+  geom_density_ridges2(stat = "density") +
+  xlim(c(0,1)) + 
+  stat_density_ridges(quantile_lines = TRUE, quantiles = 2)
+theme_classic()
+
